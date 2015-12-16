3206af48bc3c5845388f370bfab31938a4941a4a
Erin Tomson <erin@integerlabs.net>
2015-12-15 19:51:20 -0800

    Updated Temperature sketch to display the temperature on the display

--- Temperature/Temperature.ino
+++ Temperature/Temperature.ino
@@ -2,19 +2,36 @@
 #include "Modulo.h"
 
 TemperatureProbeModulo tempProbe;
+DisplayModulo display;
 
+// When the temperature changes, clear and redraw the display
+void onTemperatureChange(TemperatureProbeModulo &t) {
+    // Clear the dislay first
+    display.clear();
 
-void onTemperatureChanged(TemperatureProbeModulo &t) {
-    Serial.print(tempProbe.isValid());
-    Serial.print(" ");
-    Serial.println(tempProbe.getTemperatureC());
+    // Print the temperature in degrees C
+    display.println(tempProbe.getTemperatureC());
+
+    // Print the temperature in degrees F
+    display.println(tempProbe.getTemperatureF());
+
+    // Refresh the display with the new temperature
+    display.refresh();
 }
 
 void setup() {
-    Serial.begin(9600);
-    tempProbe.setTemperatureChangeCallback(onTemperatureChanged);
+    // Need to call Modulo.setup() before drawing the display for the first time
+    Modulo.setup();
+
+    // Register our function to be called when the temperature changes
+    tempProbe.setTemperatureChangeCallback(onTemperatureChange);
+
+    // Call the temperature change callback once here to display the temperature
+    // when the program starts up
+    onTemperatureChange(tempProbe);
 }
 
 void loop() {
+    // Handle events on each iteration of the event loop
     Modulo.loop();
 }
